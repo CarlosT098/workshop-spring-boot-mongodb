0160987265934c477398314f1c54ac204cc070a4
CarlosT098 <carlostgt1590@gmail.com>
2024-08-12 17:43:43 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/carlostakashi/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.carlostakashi.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carlostakashi.workshopmongo.domain.User;
+import com.carlostakashi.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+    
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}

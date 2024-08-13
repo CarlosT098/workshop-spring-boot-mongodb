5e6b882a0f3ff4e982a56c7f886206d889dc545c
CarlosT098 <carlostgt1590@gmail.com>
2024-08-13 17:06:59 -0300

    obtendo usuario por id

--- src/main/java/com/carlostakashi/workshopmongo/services/UserService.java
+++ src/main/java/com/carlostakashi/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.carlostakashi.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carlostakashi.workshopmongo.domain.User;
 import com.carlostakashi.workshopmongo.repository.UserRepository;
+import com.carlostakashi.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,14 @@ public class UserService {
     
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = repo.findById(id);
+        if (user.isEmpty()) {
+            throw new ObjectNotFoundException("objeto nao encontrado");
+        }
+        return user.get();
+        
     }
 }

d5cb5192bf4a9a55f67cfd0c6644430e9f571e9d
CarlosT098 <carlostgt1590@gmail.com>
2024-08-14 12:56:27 -0300

    insercao de usuario com POST

--- src/main/java/com/carlostakashi/workshopmongo/services/UserService.java
+++ src/main/java/com/carlostakashi/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carlostakashi.workshopmongo.domain.User;
+import com.carlostakashi.workshopmongo.dto.UserDTO;
 import com.carlostakashi.workshopmongo.repository.UserRepository;
 import com.carlostakashi.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -28,4 +29,13 @@ public class UserService {
         return user.get();
         
     }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
 }
